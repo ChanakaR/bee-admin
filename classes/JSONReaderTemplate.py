258006108cb310c51223ace9efd9c144c0cb6a73
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-11-07 11:30:45 +0530

    removed teminal priniting - unncessary prints

--- classes/JSONReaderTemplate.py
+++ classes/JSONReaderTemplate.py
@@ -3,8 +3,7 @@ import os
 
 class JSONReaderTemplate:
     json_file_path = os.path.dirname(os.path.realpath(__file__)) + "/../config/"
-    print(os.path.dirname(os.path.realpath(__file__)))
-
+    
     def getJSONContent(self,file_name):
         file_full_path = self.json_file_path + file_name
         with open(file_full_path) as f :
